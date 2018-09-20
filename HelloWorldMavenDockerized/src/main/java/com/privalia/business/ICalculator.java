e39b9fad701e68e6d8a31821d9c0e97e7ad5847d
cram6blue <43406639+cram6blue@users.noreply.github.com>
2018-09-20 12:33:58 +0200

    Add files via upload
    "Closes #1"

---
+++ HelloWorldMavenDockerized/src/main/java/com/privalia/business/ICalculator.java
@@ -0,0 +1,13 @@
+package com.privalia.business;
+
+public interface ICalculator {
+	int sum(int num1, int num2);// els mètodes en una interfase son publics per
+								// defecte//
+
+	int substract(int num1, int num2);
+
+	int multiply(int num1, int num2);
+
+	int divide(int num1, int num2);
+
+}
